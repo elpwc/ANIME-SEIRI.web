45570dfa0e8bca73c64dada431b084ad210a9b29
elpwc <elpwc@hotmail.com>
2021-09-18 02:03:13 +0800

    mami

---
+++ new.php
@@ -0,0 +1,19 @@
+<?php
+require "dbcfg.php";
+
+$name_zh_cn = $_POST['comment-name'];
+$name_jp = $_POST['anime_name_jp'];
+$name_en = $_POST['anime_name_en'];
+$ = $_POST['anime_'];
+$ = $_POST['anime_'];
+$ = $_POST['anime_'];
+$ = $_POST['anime_'];
+$ = $_POST['anime_'];
+
+$link = @mysqli_connect(HOST, USER, PASS, DBNAME) or die("提示：数据库连接失败！");
+//mysqli_select_db($link, DBNAME);
+mysqli_set_charset($link, 'utf8');
+
+$sql = "INSERT INTO messages (name, text) VALUES ('".$comment_name."','".$comment_text."');";
+
+$result = mysqli_query($link, $sql);
